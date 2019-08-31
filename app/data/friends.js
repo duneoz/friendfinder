1f56773a0d7a8e2a22771c7af0c78c8046f84121
duneoz <nicholas.charles.meyers@gmail.com>
2019-08-30 21:48:51 -0700

    fixed the jquery links to get this thing to run properly

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Nick Meyers",
+		photo: "https://assets.mubi.com/images/cast_member/48666/image-w240.jpg?1512808456",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Nick Meyers",
+		photo: "https://twitter.com/NickMeyers214/photo",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Nick Meyers",
+		photo: "https://radiusco.work/wp-content/uploads/2019/06/Nick-Meyers-photo-2-600x601.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Nick Meyers",
+		photo: "https://i1.sndcdn.com/avatars-000100050678-eyfods-t500x500.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Nick Meyers",
+		photo: "https://s3-us-west-1.amazonaws.com/co-directory-images/nicholascmeyers.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
